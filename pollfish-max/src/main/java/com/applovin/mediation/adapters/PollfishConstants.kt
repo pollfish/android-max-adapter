76ac4615337ee5745042a246dfb132550f37b961
Fotis Mitropoulos <fotis@pollfish.com>
2022-12-13 18:01:04 +0200

    Pollfish Android Max Adapter v6.3.1.0

--- pollfish-max/src/main/java/com/applovin/mediation/adapters/PollfishConstants.kt
+++ pollfish-max/src/main/java/com/applovin/mediation/adapters/PollfishConstants.kt
@@ -1,8 +1,8 @@
 package com.applovin.mediation.adapters
 
 object PollfishConstants {
-    const val POLLFISH_SDK_VERSION = "6.3.0"
-    const val POLLFISH_ADAPTER_VERSION = "6.3.0.0"
+    const val POLLFISH_SDK_VERSION = "6.3.1"
+    const val POLLFISH_ADAPTER_VERSION = "6.3.1.0"
     const val REMOTE_PARAMS_KEY = "custom_parameters"
     const val POLLFISH_REQUEST_UUID_EXTRA_PARAM_KEY = "request_uuid"
     const val POLLFISH_RELEASE_MODE_EXTRA_PARAM_KEY = "release_mode"
